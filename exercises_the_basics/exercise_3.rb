d580db8215b19b3ad18fb9c8ff6ec71e8dbc41d1
Douglas Purdy <dpurdy@my.wgu.edu>
2016-05-25 06:54:15 -0400

    added exercise 3

---
+++ exercises_the_basics/exercise_3.rb
@@ -0,0 +1,9 @@
+movie_hash = {goonies: 1985,
+  willow: 1988,
+  avatar: 2009,
+  jurassic_park: 1993}
+
+puts movie_hash[:goonies]
+puts movie_hash[:willow]
+puts movie_hash[:avatar]
+puts movie_hash[:jurassic_park]
